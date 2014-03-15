8c3efed0bbd103df17c11d4c3803ea0a259baf6d
Ole Kristensen <ole@kristensen.name>
2014-03-15 17:21:26 +0100

    First wierd tree with rotation bugs

--- src/Trae.cpp
+++ src/Trae.cpp
@@ -19,6 +19,10 @@ void Trae::setup() {
     coldlight.setDiffuseColor(ofColor::white);
     coldlight.setPosition(1, -0.5, -1);
     
+    trae.set(0.05,0.5);
+    trae.setPosition(0,0.75,0);
+    trae.setResolutionRadius(20);
+    trae.make(5);
 }
 
 
@@ -33,7 +37,7 @@ void Trae::draw(int _surfaceId) {
         warmlight.enable();
         coldlight.enable();
         
-        // ofSetColor(0,255,0,250);
+/*        // ofSetColor(0,255,0,250);
         // FIXME: there is an alpha problem here, the box can't be seen through the rect if drawn
         // ofRect(-1, -1, 2, 2);
         
@@ -53,7 +57,15 @@ void Trae::draw(int _surfaceId) {
         ofEllipse(0,0,.5,.5);
         
         ofPopMatrix();
-        
+*/
+        ofSetColor(255,255,255);
+        ofPushMatrix();
+        glTranslatef(0.,0,.5);
+
+        ofRotateY(ofGetElapsedTimef()*10);
+        trae.draw();
+
+        ofPopMatrix();
         warmlight.disable();
         coldlight.disable();
         
@@ -67,6 +79,7 @@ void Trae::update() {
 
 void Trae::setGui(ofxUICanvas * gui, float width){
     ContentScene::setGui(gui, width);
+    
     
 }
 
--- src/Trae.h
+++ src/Trae.h
@@ -11,21 +11,40 @@
 #include "ofMain.h"
 
 class Branch : public ofCylinderPrimitive {
-    vector<Branch> branches;
+    vector<Branch*> branches;
     
-    void draw(){
-        this->ofCylinderPrimitive::draw();
-        for (std::vector<Branch>::iterator it = branches.begin() ; it != branches.end(); ++it) {
-            Branch b = *(it);
-            b.draw();
+    void addBranch(){
+        Branch * b = new Branch();
+        b->set( this->getRadius()*0.85, this->getHeight()*ofRandom(0.5,1.3) );
+        b->setResolutionRadius(this->getResolutionRadius());
+        b->move(0,-this->getHeight(),0);
+        b->setOrientation(ofVec3f(ofRandom(-45,45),ofRandom(-45,45),ofRandom(-45,45)));
+        b->setParent(*this);
+        branches.push_back(b);
+    }
+public:
+
+    void make(int steps){
+        if(steps > 1){
+            int numberBranches = ofRandom(2,5);
+            for(int i = 0; i < numberBranches; i++){
+                addBranch();
+            }
+            for (std::vector<Branch*>::iterator it = branches.begin() ; it != branches.end(); ++it) {
+                Branch *b = *(it);
+                b->make(steps-1);
+            }
         }
     }
     
-    void addBranch(){
-        Branch b;
-        b.set( this->getRadius()*0.95, this->getHeight()*0.85 );
-        
+    void draw(){
+        this->ofCylinderPrimitive::draw();
+        for (std::vector<Branch*>::iterator it = branches.begin() ; it != branches.end(); ++it) {
+            Branch *b = *(it);
+            b->draw();
+        }
     }
+
     
 };
 
@@ -42,5 +61,7 @@ public:
     
     ofLight warmlight;
     ofLight coldlight;
+    
+    Branch trae;
     
 };
